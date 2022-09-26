0fb03f2a1f4f98715f2dacb337abb218fde89b17
Bryce <github20210803@accounts.brycedrennan.com>
2022-09-25 23:27:03 -0700

    fix/version: 2.0.1
    quick fix for compatibility with Pillow < 9.1.0
    https://stackoverflow.com/questions/71738218/module-pil-has-not-attribute-resampling

--- imaginairy/__init__.py
+++ imaginairy/__init__.py
@@ -2,6 +2,7 @@ import os.path
 
 # tells pytorch to allow MPS usage (for Mac M1 compatibility)
 os.putenv("PYTORCH_ENABLE_MPS_FALLBACK", "1")
+import PIL.Image  # noqa
 
 from .api import imagine, imagine_image_files  # noqa
 from .enhancers.describe_image_blip import generate_caption  # noqa
@@ -11,5 +12,9 @@ from .schema import (  # noqa
     LazyLoadingImage,
     WeightedPrompt,
 )
+
+# https://stackoverflow.com/questions/71738218/module-pil-has-not-attribute-resampling
+if not hasattr(PIL.Image, "Resampling"):  # Pillow<9.0
+    PIL.Image.Resampling = PIL.Image
 
 PKG_ROOT = os.path.dirname(__file__)
