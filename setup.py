26a504fef4dacf8452d482ea38cb6026a0cdb41a
Bryce <github20210803@accounts.brycedrennan.com>
2022-12-02 02:05:12 -0800

    version: 7.0.0
    Major version because new images have negative prompt by default.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="imaginAIry",
     author="Bryce Drennan",
     # author_email="b r y p y d o t io",
-    version="6.1.2",
+    version="7.0.0",
     description="AI imagined images. Pythonic generation of stable diffusion images.",
     long_description=readme,
     long_description_content_type="text/markdown",
