7b684b6e5c9dcf6383beb87e56fead3f80e6f6b8
Bryce <github20210803@accounts.brycedrennan.com>
2023-02-18 15:40:04 -0800

    test: add smoke test for edit command

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -1,7 +1,7 @@
 import pytest
 from click.testing import CliRunner
 
-from imaginairy.cmds import imagine_cmd
+from imaginairy.cmds import edit_image, imagine_cmd
 from imaginairy.utils import get_device
 from tests import TESTS_FOLDER
 
@@ -19,6 +19,22 @@ def test_imagine_cmd():
             f"{TESTS_FOLDER}/test_output",
             "--seed",
             "703425280",
+        ],
+    )
+    assert result.exit_code == 0
+
+
+@pytest.mark.skipif(get_device() == "cpu", reason="Too slow to run on CPU")
+def test_edit_cmd():
+    runner = CliRunner()
+    result = runner.invoke(
+        edit_image,
+        [
+            f"{TESTS_FOLDER}/data/dog.jpg",
+            "--steps",
+            "1",
+            "-p",
+            "turn the dog into a cat",
         ],
     )
     assert result.exit_code == 0
