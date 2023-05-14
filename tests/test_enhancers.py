487d3509ed3f33eede3b0324f12790a44e396458
Bryce <github20210803@accounts.brycedrennan.com>
2023-05-13 23:30:02 -0700

    tests: update tests
    - controlnet version changes + graphics card change

--- tests/test_enhancers.py
+++ tests/test_enhancers.py
@@ -134,11 +134,13 @@ def test_clip_mask_parser(mask_text, expected):
 
 @pytest.mark.skipif(get_device() == "cpu", reason="Too slow to run on CPU")
 def test_describe_picture():
+    seed_everything(1)
     img = Image.open(f"{TESTS_FOLDER}/data/girl_with_a_pearl_earring.jpg")
     caption = generate_caption(img)
     assert caption in {
         "a painting of a girl with a pearl earring wearing a yellow dress and a pearl earring in her ear and a black background",
         "a painting of a girl with a pearl ear wearing a yellow dress and a pearl earring on her left ear and a black background",
+        "a painting of a woman with a pearl ear wearing an ornament pearl earring and wearing an orange, white, blue and yellow dress",
     }
 
 
