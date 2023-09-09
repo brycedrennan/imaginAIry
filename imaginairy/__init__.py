856d12413596832bee33a28ea70446126d1fb285
Bryce <github20210803@accounts.brycedrennan.com>
2023-09-09 14:50:46 -0700

    build: better communicate lack of support for Python 3.11

--- imaginairy/__init__.py
+++ imaginairy/__init__.py
@@ -3,6 +3,8 @@ import os
 # tells pytorch to allow MPS usage (for Mac M1 compatibility)
 os.putenv("PYTORCH_ENABLE_MPS_FALLBACK", "1")
 
+import sys  # noqa
+
 from .api import imagine, imagine_image_files  # noqa
 from .schema import (  # noqa
     ImaginePrompt,
@@ -10,3 +12,12 @@ from .schema import (  # noqa
     LazyLoadingImage,
     WeightedPrompt,
 )
+
+# if python version is 3.11 or higher, throw an exception
+if sys.version_info >= (3, 11):
+    msg = (
+        "Imaginairy is not compatible with Python 3.11 or higher. Please use Python 3.8 - 3.10.\n"
+        "This is due to torch 1.13 not supporting Python 3.11 and this library not having yet switched "
+        "to torch 2.0"
+    )
+    raise RuntimeError(msg)
